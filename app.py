561cac39a4f523e12ad74a9c1db711206c0edbc5
bzww323 <146072302+bzww323@users.noreply.github.com>
2025-03-08 15:01:37 +0300

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import requests
 # st.success(f'{x * rate:,.2f}') # :,.2f - разделитель между цифрами - запятая, берем их до 2 цифр после запятой
 
 # динамически берем курс валюты 1 раз в сутки
-@st.cache_data(ttl="1 day")
+@st.cache_data(ttl=1)
 def get_rates():
     url = "https://open.er-api.com/v6/latest/RUB"
     inverse_rates = requests.get(url).json()["rates"]
@@ -25,6 +25,13 @@ col1, col2 = st.columns(2)
 x = col1.number_input("", min_value=0.0, value=1.0, step=1.0)
 st.write(x)
 rates = get_rates()
+# исключаем перевод рублей в рубли
+if "RUB" in rates:
+    del rates["RUB"]
+# cоздаем список валют с приоритетом для USD и EUR
+priority_currencies = ["USD", "EUR"]
+other_currencies = sorted([x for x in rates.keys() if x not in priority_currencies])
+currencies = priority_currencies + other_currencies
 currency = col2.selectbox("Валюта", list(rates))
 # st.write(f'{x * rate:,.2f}') # :,.2f - разделитель между цифрами - запятая, берем их до 2 цифр после запятой
 # st.write(f'{x * rate:,.2f}') # :,.2f - разделитель между цифрами - запятая, берем их до 2 цифр после запятой
